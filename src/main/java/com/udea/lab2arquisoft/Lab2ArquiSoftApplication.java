f6688fb724799c29c09babc2aab73492628d53d2
Jaime Muñoz <jaime.munozq@udea.edu.co>
2024-03-21 17:59:57 -0500

    This is my first commit

---
+++ src/main/java/com/udea/lab2arquisoft/Lab2ArquiSoftApplication.java
@@ -0,0 +1,13 @@
+package com.udea.lab2arquisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2ArquiSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2ArquiSoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/lab2arquisoft/Lab2ArquiSoftApplicationTests.java
@@ -0,0 +1,51 @@
+package com.udea.lab2arquisoft;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+@SpringBootTest
+class Lab2ArquiSoftApplicationTests {
+
+	@Autowired
+	private DataController dataController;
+
+	@Test
+	public void health() {
+		String response = dataController.healthCheck();
+		assertEquals("app works perfectly!", response);
+	}
+
+	@Test
+	public void version() {
+		String response = dataController.version();
+		assertEquals("The version of the app is 1.0.0", response);
+	}
+
+	@Test
+	public void nationLength() {
+		JsonNode response = dataController.getRandomNations();
+		assertEquals(10, response.size());
+	}
+
+	@Test
+	public void currencyLength() {
+		JsonNode response = dataController.getRandomCurrencies();
+		assertEquals(20, response.size());
+	}
+
+	@Test
+	public void airlinesLength() {
+		JsonNode response = dataController.getRandomAirlines();
+		assertEquals(20, response.size());
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+}
